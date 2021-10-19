63ddec1a9fa9e0862f51c61d9fe80682c58d3975
Spenser Hale <spenserhale@outlook.com>
2021-10-18 22:23:09 -0700

    feat(MadLibsGame): adding age check + language censor

--- src/MadLibsGame.java
+++ src/MadLibsGame.java
@@ -7,9 +7,24 @@ public class MadLibsGame {
             System.exit(1);
         }
 
+        int age = Integer.parseInt(console.readLine("Enter age:  "));
+        if(age <= 13) {
+            console.printf("Sorry you must be older than 13.\n");
+            System.exit(0);
+        }
+
         String name = console.readLine("Enter a name:  ");
         String adjective = console.readLine("Enter an adjective:  ");
-        String noun = console.readLine("Enter a noun:  ");
+        String noun;
+        boolean isNounInvalid;
+        do {
+            noun = console.readLine("Enter a noun:  ");
+            isNounInvalid = name.equalsIgnoreCase("dork") || name.equalsIgnoreCase("jerk");
+            if(isNounInvalid) {
+                console.printf("That language is not allowed. Try again. \n");
+            }
+        } while (isNounInvalid);
+
         String adverb = console.readLine("Enter an adverb:  ");
         String verb = console.readLine("Enter a verb ending with -ing:  ");
 
