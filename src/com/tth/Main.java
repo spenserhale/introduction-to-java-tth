5255a306775a98f2f44508591945675506759db1
Spenser Hale <spenserhale@outlook.com>
2021-10-20 00:14:10 -0700

    feat(Maps): creating Map example

--- src/com/tth/Main.java
+++ src/com/tth/Main.java
@@ -1,12 +1,34 @@
 package com.tth;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
+    public static final String BREAKFAST = "breakfast";
+    public static final String LUNCH = "lunch";
+    public static final String DINNER = "dinner";
+
     public static void main(String[] args) {
-        listExamples();
+        mapExamples();
+    }
+
+    private static void mapExamples() {
+        Map<String, String> meals = new HashMap<>();
+        meals.put(BREAKFAST, "Waffles");
+        meals.put(LUNCH, "Gyros");
+        meals.put(DINNER, "Enchiladas");
+
+        System.out.println(meals.get(DINNER));
+
+        String lunch = meals.remove(LUNCH);
+        boolean hasLunch = meals.containsKey(LUNCH);
+        boolean hasGyros = meals.containsValue("Gyros");
+        int size = meals.size();
+
+        System.out.println(lunch + " " + hasLunch + " " + hasGyros + " " + size);
     }
 
     private static void listExamples() {
