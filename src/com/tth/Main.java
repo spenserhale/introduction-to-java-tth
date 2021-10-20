d6b36758e706cfe210083d4250658e07f9e7f0cb
Spenser Hale <spenserhale@outlook.com>
2021-10-20 00:03:55 -0700

    feat(Lists): creating List examples

--- src/com/tth/Main.java
+++ src/com/tth/Main.java
@@ -1,8 +1,44 @@
 package com.tth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        listExamples();
+    }
+
+    private static void listExamples() {
+        List<String> groceryLine = new ArrayList<>();
+
+        groceryLine.add("Jerome");
+        groceryLine.add("Beth");
+        groceryLine.add("Sam");
+        groceryLine.add("Tom");
+        groceryLine.add("Harry");
+
+        System.out.println(groceryLine);
+
+        groceryLine.remove("Beth");
+        groceryLine.remove(1);
+
+        System.out.println(groceryLine);
+
+        String jerome = groceryLine.get(0);
+        System.out.println(jerome);
+
+        int tomIndex = groceryLine.indexOf("Tom");
+        System.out.println(tomIndex);
+
+        System.out.println(groceryLine.size());
+
+        for (String name : groceryLine) {
+            System.out.println(name);
+        }
+    }
+
+    private static void genericsExamples() {
         Milk milk = new Milk();
         Oranges oranges = new Oranges();
 
