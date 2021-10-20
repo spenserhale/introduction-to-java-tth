83accfa9672198b9e71842f0129d39c9fcdb4335
Spenser Hale <spenserhale@outlook.com>
2021-10-19 23:50:27 -0700

    feat(Generics): creating method example

--- src/com/tth/Main.java
+++ src/com/tth/Main.java
@@ -9,11 +9,16 @@ public class Main {
         Box<Milk> boxOfMilk = new Box<>();
         Box<Oranges> boxOfOranges = new Box<>();
 
-        boxOfMilk.add(milk);
-        boxOfOranges.add(oranges);
+        debugAdd(milk, boxOfMilk);
+        debugAdd(oranges, boxOfOranges);
 
         boxOfMilk.remove().drink();
         boxOfOranges.remove().juggle();
+    }
+
+    static <T> void debugAdd(T item, Box<T> box) {
+        System.out.println("Type: " + item.getClass().getSimpleName());
+        box.add(item);
     }
 
     private static void interfaceExamples() {
